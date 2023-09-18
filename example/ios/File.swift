bc92632ba053a817d8fb6373572156992dd55c0c
“c.cinar” <cagricinar@gmail.com>
2023-09-18 12:45:16 +0300

    remove: project

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  EnquraExample
-//
-
-import Foundation
--- ios/Enqura.swift
+++
@@ -1,8 +0,0 @@
-@objc(Enqura)
-class Enqura: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
